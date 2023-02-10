dd936026dee4b0ee4c1fc904e39963994a936440
FUGAMARU <gofugashi@gmail.com>
2023-02-10 21:53:48 +0900

    [Update] Socket.IOの接続先パスを環境変数から指定できるように

--- contexts/SocketIO.js
+++ contexts/SocketIO.js
@@ -1,5 +1,7 @@
 import { createContext } from "react"
 import { io } from "socket.io-client"
 
-export const socket = io(process.env.NEXT_PUBLIC_WEBSOCKET_ADDRESS)
+export const socket = io(process.env.NEXT_PUBLIC_WEBSOCKET_ADDRESS, {
+  path: process.env.NEXT_PUBLIC_WEBSOCKET_PATH
+})
 export const SocketContext = createContext()
